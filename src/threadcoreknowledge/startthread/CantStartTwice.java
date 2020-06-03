f22112c0446bd4f4f32760e9f4d99cf3f930df78
durex3 <liugelong666@163.com>
2020-06-03 21:18:48 +0800

    启动多线程的正确姿势

---
+++ src/threadcoreknowledge/startthread/CantStartTwice.java
@@ -0,0 +1,14 @@
+package threadcoreknowledge.startthread;
+
+/**
+ * @author gelong
+ * @date 2020/6/1 23:09
+ */
+public class CantStartTwice {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread();
+        thread.start();
+        thread.start();
+    }
+}
---
+++ src/threadcoreknowledge/startthread/StartAndRunMethod.java
@@ -0,0 +1,16 @@
+package threadcoreknowledge.startthread;
+
+/**
+ * @author gelong
+ * @date 2020/6/1 21:39
+ */
+public class StartAndRunMethod {
+
+    public static void main(String[] args) {
+        Runnable runnable = () -> {
+            System.out.println(Thread.currentThread().getName());
+        };
+        runnable.run();
+        new Thread(runnable).start();
+    }
+}
